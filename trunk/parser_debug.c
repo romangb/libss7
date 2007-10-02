d0659399b14005bf01926ff49ea90a7d03648d91
mattf <mattf@caa0ee0b-16e0-41cb-a6fa-51cd4f70227f>
2007-10-02 21:28:35 +0000

    Add a parser debug utility

---
+++ trunk/parser_debug.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "libss7.h"
+#include "ss7_internal.h"
+#include "mtp2.h"
+#include "isup.h"
+#include "mtp3.h"
+
+int main(int argc, char **argv)
+{
+	FILE *fp;
+	struct ss7 *ss7 = ss7_new(SS7_ANSI);
+	unsigned char mybuf[512];
+	int res = 0, i = 0, size;
+
+	if (argc != 2)
+		return -1;
+
+	fp = fopen(argv[1], "r");
+
+	while (res != EOF) {
+		res = fscanf(fp, "%x ", &mybuf[i++]);
+	}
+
+	size = i - 1;
+
+	for (i = 0; i < size; i++) {
+		printf("%.2x ", mybuf[i]);
+	}
+
+	printf("\n");
+
+	ss7_add_link(ss7, 10);
+
+	ss7->debug = SS7_DEBUG_MTP2 | SS7_DEBUG_MTP3 | SS7_DEBUG_ISUP;
+	ss7->links[0]->state = MTP_INSERVICE;
+
+	mtp2_receive(ss7->links[0], mybuf, size);
+
+	return 0;
+}
