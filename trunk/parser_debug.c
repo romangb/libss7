5055d2bded5428d122c59e11eba044cd338b72a1
mattf <mattf@caa0ee0b-16e0-41cb-a6fa-51cd4f70227f>
2007-10-05 21:24:07 +0000

    Make it easier to switch ss7 types

--- trunk/parser_debug.c
+++ trunk/parser_debug.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "libss7.h"
 #include "ss7_internal.h"
 #include "mtp2.h"
@@ -8,16 +9,26 @@
 int main(int argc, char **argv)
 {
 	FILE *fp;
-	struct ss7 *ss7 = ss7_new(SS7_ANSI);
+	struct ss7 *ss7;
 	unsigned char mybuf[512];
 	unsigned int tmp;
+	int ss7type;
 	int res = 0, i = 0, size;
 	ss7_event *e;
 
-	if (argc != 2)
+	if (argc != 3)
 		return -1;
 
-	fp = fopen(argv[1], "r");
+	if (!strcasecmp(argv[1], "ansi"))
+		ss7type = SS7_ANSI;
+	else if (!strcasecmp(argv[1], "itu"))
+		ss7type = SS7_ITU;
+	else
+		return -1;
+
+	ss7 = ss7_new(ss7type);
+
+	fp = fopen(argv[2], "r");
 
 	while (res != EOF) {
 		res = fscanf(fp, "%x ", &tmp);
