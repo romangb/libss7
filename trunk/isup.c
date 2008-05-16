b02fb5fbd3ef2faa8dfc1b10a5ab4d48316b0e0c
mattf <mattf@caa0ee0b-16e0-41cb-a6fa-51cd4f70227f>
2008-05-16 16:42:08 +0000

    Fixed bug in functions :-)

--- trunk/isup.c
+++ trunk/isup.c
@@ -657,10 +657,10 @@ static FUNC_DUMP(opt_backward_call_ind_dump)
 	c = (parm[0] >> 2) & 1;
 	d = (parm[0] >> 3) & 1;
 
-	ss7_message("\t\t\tIn-band information indicator: %d\n", a);
-	ss7_message("\t\t\tCall diversion may occur indicator: %d\n", b);
-	ss7_message("\t\t\tSimple segmentation indicator: %d\n", c);
-	ss7_message("\t\t\tMLPP user indicator: %d\n", d);
+	ss7_message(ss7, "\t\t\tIn-band information indicator: %d\n", a);
+	ss7_message(ss7, "\t\t\tCall diversion may occur indicator: %d\n", b);
+	ss7_message(ss7, "\t\t\tSimple segmentation indicator: %d\n", c);
+	ss7_message(ss7, "\t\t\tMLPP user indicator: %d\n", d);
 	return 1;
 }
 
